484d78c5bf8984db43997db292fe38a07bf66e42
Lukas Huth <60547284+LukasHuth@users.noreply.github.com>
2023-06-01 08:52:08 +0000

    Fixed AST free

--- src/AST.c
+++ src/AST.c
@@ -1,5 +1,7 @@
 #include "AST.h"
 
+const bool DEBUG = true;
+
 AST *AST_new(AST ast)
 {
     AST *ptr = malloc(sizeof(AST));
@@ -251,54 +253,73 @@ void AST_print(AST *ast)
 }
 void AST_free(AST *ast)
 {
-//     switch (ast->tag)
-//     {
-//     case AST_NUMBER:
-//         break;
-//     case AST_ADD:
-//     case AST_SUB:
-//     case AST_DIV:
-//     case AST_MUL:
-//         AST_free(ast->data.AST_TUPLE.left);
-//         AST_free(ast->data.AST_TUPLE.right);
-//         break;
-//     case AST_EXPR:
-//         AST_free(ast->data.AST_EXPR.expr);
-//         break;
-//     case AST_RETURN:
-//         AST_free(ast->data.AST_RETURN.expr);
-//         break;
-//     case AST_ARGUMENT:
-//         AST_free(ast->data.AST_ARGUMENT.type);
-//         break;
-//     case AST_FUNCTION:
-//         AST_free(ast->data.AST_FUNCTION.type);
-//         AST_free(ast->data.AST_FUNCTION.body);
-//         for (size_t i = 0; i < ast->data.AST_FUNCTION.array_size; i++)
-//         {
-//             AST_free(ast->data.AST_FUNCTION.arguments[i]);
-//         }
-//         free(ast->data.AST_FUNCTION.arguments);
-//         break;
-//     case AST_NODE:
-//         for (size_t i = 0; i < ast->data.AST_NODE.array_size; i++)
-//         {
-//             AST_free(ast->data.AST_NODE.children[i]);
-//         }
-//         free(ast->data.AST_NODE.children);
-//         break;
-//     case AST_TYPE:
-//         break;
-//     case AST_DECLARATION:
-//         AST_free(ast->data.AST_DECLARATION.type);
-//         AST_free(ast->data.AST_DECLARATION.value);
-//         break;
-//     case AST_ASSIGN:
-//         AST_free(ast->data.AST_ASSIGN.value);
-//         break;
-//     default:
-//         printf("AST(free): Error: Unknown tag (%s)\n", get_tag_name(ast->tag));
-//         break;
-//     }
+    switch (ast->tag)
+    {
+    case AST_RETURN:
+        AST_free(ast->data.AST_RETURN.expr);
+        if(DEBUG) printf("free return\n");
+        break;
+    case AST_NODE:
+        for (size_t i = 0; i < ast->data.AST_NODE.array_size; i++)
+        {
+            AST_free(ast->data.AST_NODE.children[i]);
+        }
+        free(ast->data.AST_NODE.children);
+        if(DEBUG) printf("free node\n");
+        break;
+    case AST_FUNCTION:
+        AST_free(ast->data.AST_FUNCTION.type);
+        AST_free(ast->data.AST_FUNCTION.body);
+        for (size_t i = 0; i < ast->data.AST_FUNCTION.array_size; i++)
+        {
+            AST_free(ast->data.AST_FUNCTION.arguments[i]);
+        }
+        free(ast->data.AST_FUNCTION.arguments);
+        if(DEBUG) printf("free function\n");
+        break;
+    case AST_CALL:
+        for (size_t i = 0; i < ast->data.AST_CALL.array_size; i++)
+        {
+            AST_free(ast->data.AST_CALL.arguments[i]);
+        }
+        free(ast->data.AST_CALL.arguments);
+        if(DEBUG) printf("free call\n");
+        break;
+    case AST_ARGUMENT:
+        AST_free(ast->data.AST_ARGUMENT.type);
+        if(DEBUG) printf("free argument\n");
+        break;
+    case AST_TYPE:
+        if(DEBUG) printf("free type\n");
+        break;
+    case AST_DECLARATION:
+        AST_free(ast->data.AST_DECLARATION.type);
+        AST_free(ast->data.AST_DECLARATION.value);
+        if(DEBUG) printf("free declaration\n");
+        break;
+    case AST_ASSIGN:
+        AST_free(ast->data.AST_ASSIGN.value);
+        if(DEBUG) printf("free assign\n");
+        break;
+    case AST_IF:
+        AST_free(ast->data.AST_IF.condition);
+        AST_free(ast->data.AST_IF.body);
+        if(DEBUG) printf("free if\n");
+        break;
+    case AST_WHILE:
+        AST_free(ast->data.AST_WHILE.condition);
+        AST_free(ast->data.AST_WHILE.body);
+        if(DEBUG) printf("free while\n");
+        break;
+    case AST_FOR:
+        AST_free(ast->data.AST_FOR.init);
+        AST_free(ast->data.AST_FOR.condition);
+        AST_free(ast->data.AST_FOR.increment);
+        AST_free(ast->data.AST_FOR.body);
+        if(DEBUG) printf("free for\n");
+        break;
+    default:
+        break;
+    }
     free(ast);
 }
