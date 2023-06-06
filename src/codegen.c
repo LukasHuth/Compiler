b0a1f7bb65fd1a7d3cf61f6730da85a85d7bd44e
Lukas Huth <60547284+LukasHuth@users.noreply.github.com>
2023-06-06 13:05:14 +0000

    Fixed an error that caused gcc to stop compiling the code. The Error was caused by a misstyped approach.
    Added a new struct to the codegen.c file.
    It is used to store the information about the current function.

--- src/codegen.c
+++ src/codegen.c
@@ -1,12 +1,35 @@
 #include "headers/codegen.h"
 
-LLVMValueRef generate_binary_expression(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables);
-LLVMValueRef generate_expression(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables);
-void generate_return(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables);
-void generate_function_body(AST *ast, LLVMBuilderRef builder);
-void generate_variable_declaration(AST *ast, LLVMBuilderRef builder, LLVMValueRef* variables);
-void generate_variable_assignment(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables);
-LLVMValueRef generate_function_call(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables);
+#define InternalInfo struct INTERNAL_INFO
+
+struct INTERNAL_INFO
+{
+    size_t temp_count;
+    LLVMBuilderRef builder;
+    LLVMValueRef function;
+    LLVMValueRef* variables;
+    LLVMModuleRef module;
+    size_t variable_count;
+};
+
+bool isValidInfo(InternalInfo *info)
+{
+    return (
+        info != NULL &&
+        info->builder != NULL &&
+        info->variables != NULL &&
+        info->function != NULL &&
+        info->module != NULL
+    );
+}
+
+void generate_function_body(AST *ast, InternalInfo *info);
+LLVMValueRef generate_binary_expression(AST *ast, InternalInfo *info);
+LLVMValueRef generate_expression(AST *ast, InternalInfo *info);
+void generate_return(AST *ast, InternalInfo *info);
+void generate_variable_declaration(AST *ast, InternalInfo *info);
+void generate_variable_assignment(AST *ast, InternalInfo *info);
+LLVMValueRef generate_function_call(AST *ast, InternalInfo *info);
 void generate_function(AST *ast, LLVMModuleRef module, LLVMBuilderRef builder);
 
 void codegen_generate(CODEGEN *codegen, LLVMModuleRef module, LLVMBuilderRef builder);
@@ -95,8 +118,6 @@ void generate_function(AST *ast, LLVMModuleRef module, LLVMBuilderRef builder)
         // char* type_name = type->data.AST_TYPE.name;
         param_types[i] = LLVMInt32Type();
     }
-    // TODO: generate variables that contain the arguments
-    // fprintf(file, ") {\n");
     LLVMTypeRef function_type = LLVMFunctionType(LLVMInt32Type(), param_types, param_count, false);
     LLVMValueRef function = LLVMAddFunction(module, function_name, function_type);
     LLVMSetLinkage(function, LLVMExternalLinkage);
@@ -104,60 +125,83 @@ void generate_function(AST *ast, LLVMModuleRef module, LLVMBuilderRef builder)
     LLVMBasicBlockRef entry = LLVMAppendBasicBlock(function, "entry");
     LLVMPositionBuilderAtEnd(builder, entry);
 
-    generate_function_body(ast->data.AST_FUNCTION.body, builder);
-    // fprintf(file, "}\n");
+    /*// only needed if i dont implement the index of the arguments in the AST
+    for(size_t i = 0; i < param_count; i++)
+    {
+        AST *child = ast->data.AST_FUNCTION.arguments[i];
+        char* name = child->data.AST_ARGUMENT.name;
+        LLVMValueRef variable = LLVMBuildAlloca(builder, LLVMInt32Type(), name);
+        LLVMSetAlignment(variable, 4);
+        LLVMValueRef argument = LLVMGetParam(function, i);
+        LLVMBuildStore(builder, argument, variable);
+    }
+    */
+    InternalInfo *info = malloc(sizeof(InternalInfo));
+    info->builder = builder;
+    info->temp_count = 0;
+    info->variables = NULL;
+    info->variable_count = 0;
+    info->function = function;
+    info->module = module;
+    generate_function_body(ast->data.AST_FUNCTION.body, info);
+    free(info->variables);
+    free(info);
 }
-void generate_function_body(AST *ast, LLVMBuilderRef builder)
+void generate_function_body(AST *ast, InternalInfo *info)
 {
     if(ast == NULL){printf("AST is NULL\n");return;}
-    if(builder == NULL){printf("Builder is NULL\n");return;}
+    if(info->variables != NULL) free(info->variables);
+    info->variables = calloc(0, sizeof(LLVMValueRef));
+    if(!isValidInfo(info)){printf("Info is invalid (function_body)\n");return;}
     if(ast->tag != AST_NODE) return;
     if(ast->data.AST_NODE.array_size == 0) return;
     if(ast->data.AST_NODE.children == NULL) return;
-    size_t* temp_count = calloc(1, sizeof(size_t));
-    *temp_count = 0;
-    LLVMValueRef* variables = calloc(0, sizeof(LLVMValueRef));
+
+    info->temp_count = 0;
+    info->variable_count = 0;
+
     for(size_t i = 0; i < ast->data.AST_NODE.array_size; i++)
     {
         AST *child = ast->data.AST_NODE.children[i];
         switch (child->tag)
         {
         case AST_RETURN:
-            generate_return(child, temp_count, builder, variables);
+            generate_return(child, info);
             break;
         case AST_DECLARATION:
-            generate_variable_declaration(child, builder, variables);
+            generate_variable_declaration(child, info);
             break;
         case AST_ASSIGN:
-            generate_variable_assignment(child, temp_count, builder, variables);
+            generate_variable_assignment(child, info);
             break;
         case AST_CALL:
-            generate_function_call(child, temp_count, builder, variables);
+            generate_function_call(child, info);
             break;
         default:
             printf("Unknown tag: %d\n", child->tag);
             break;
         }
     }
-    free(variables);
-    free(temp_count);
 }
-LLVMValueRef generate_function_call(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables)
+LLVMValueRef generate_function_call(AST *ast, InternalInfo *info)
 {
+    if(!isValidInfo(info)){printf("Info is invalid(function call)\n");return NULL;}
     if(ast == NULL){printf("AST is NULL\n");return NULL;}
-    if(builder == NULL){printf("Builder is NULL\n");return NULL;}
-    if(variables == NULL){printf("Variables is NULL\n");return NULL;}
     if(ast->tag != AST_CALL){printf("AST is not a call\n");return NULL;}
     char *function_name = ast->data.AST_CALL.name;
     AST **arguments = ast->data.AST_CALL.arguments;
-    LLVMModuleRef module = LLVMGetGlobalParent(builder);
-    LLVMValueRef function = LLVMGetNamedFunction(module, function_name);
+    size_t argc = ast->data.AST_CALL.array_size;
+    LLVMValueRef function = LLVMGetNamedFunction(info->module, function_name);
     LLVMValueRef* args = calloc(ast->data.AST_CALL.array_size, sizeof(LLVMValueRef));
-    for(size_t i = 0; i < ast->data.AST_CALL.array_size; i++)
+    for(size_t i = 0; i < argc; i++)
     {
-        args[i] = generate_expression(arguments[i], temp_count, builder, variables);
+        args[i] = generate_expression(arguments[i], info);
+        if(args[i] == NULL) return NULL;
     }
-    LLVMValueRef result = LLVMBuildCall(builder, function, args, ast->data.AST_CALL.array_size, "call");
+    char* temp_name = calloc(10, sizeof(char));
+    sprintf(temp_name, "_t%ld", info->temp_count++);
+    temp_name = realloc(temp_name, sizeof(char) * (strlen(temp_name) + 1));
+    LLVMValueRef result = LLVMBuildCall(info->builder, function, args, argc, temp_name);
     free(args);
     return result;
     // fprintf(file, "%%t%ld = call i32 @%s(", *temp_count, function_name);
@@ -176,53 +220,52 @@ LLVMValueRef generate_function_call(AST *ast, size_t* temp_count, LLVMBuilderRef
     // }
     // free(arguments);
 }
-void generate_variable_assignment(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables)
+void generate_variable_assignment(AST *ast, InternalInfo *info)
 {
     if(ast == NULL){printf("AST is NULL\n");return;}
-    if(builder == NULL){printf("Builder is NULL\n");return;}
-    if(variables == NULL){printf("Variables is NULL\n");return;}
+    if(!isValidInfo(info)){printf("Info is invalid(variable assignment)\n");return;}
     if(ast->tag != AST_ASSIGN){printf("AST is not an assignment\n");return;}
     char* name = ast->data.AST_ASSIGN.name;
     AST *expr = ast->data.AST_ASSIGN.value;
-    LLVMValueRef value = generate_expression(expr, temp_count, builder, variables);
+    LLVMValueRef value = generate_expression(expr, info);
+    if(value == NULL) return;
     // value = iterate vars
     LLVMValueRef variable = NULL;
-    for(size_t i = 0; i < sizeof(variables); i++)
+    for(size_t i = 0; i < info->variable_count; i++)
     {
-        if(strcmp(name, LLVMGetValueName(variables[i])) == 0)
+        if(strcmp(name, LLVMGetValueName(info->variables[i])) == 0)
         {
-            variable = variables[i];
+            variable = info->variables[i];
             break;
         }
     }
-    LLVMBuildStore(builder, value, variable);
+    LLVMBuildStore(info->builder, value, variable);
 }
-void generate_variable_declaration(AST *ast, LLVMBuilderRef builder, LLVMValueRef* variables)
+void generate_variable_declaration(AST *ast, InternalInfo *info)
 {
     if(ast == NULL){printf("AST is NULL\n");return;}
-    if(builder == NULL){printf("Builder is NULL\n");return;}
-    if(variables == NULL){printf("Variables is NULL\n");return;}
-    LLVMValueRef value = LLVMBuildAlloca(builder, LLVMInt32Type(), ast->data.AST_DECLARATION.name);
+    if(!isValidInfo(info)){printf("Info is invalid(variable_declaration)\n");return;}
+    LLVMValueRef value = LLVMBuildAlloca(info->builder, LLVMInt32Type(), ast->data.AST_DECLARATION.name);
     LLVMSetInitializer(value, LLVMConstInt(LLVMInt32Type(), 0, 0));
-    size_t var_size = sizeof(variables);
-    variables = realloc(variables, sizeof(LLVMValueRef) * (var_size + 1));
-    variables[var_size] = value;
+    info->variable_count++;
+    info->variables = realloc(info->variables, sizeof(LLVMValueRef) * info->variable_count);
+    info->variables[info->variable_count - 1] = value;
 }
-void generate_return(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables)
+void generate_return(AST *ast, InternalInfo* info)
 {
     if(ast == NULL) return;
     if(ast->data.AST_RETURN.expr == NULL) return;
-    if(builder == NULL) return;
+    if(!isValidInfo(info)) return;
     AST *expr = ast->data.AST_RETURN.expr;
-    LLVMValueRef value = generate_expression(expr, temp_count, builder, variables);
+    LLVMValueRef value = generate_expression(expr, info);
     if(value == NULL) return;
-    LLVMBuildRet(builder, value);
+    LLVMBuildRet(info->builder, value);
 }
-LLVMValueRef generate_expression(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables)
+LLVMValueRef generate_expression(AST *ast, InternalInfo *info)
 {
     // if(!file_and_ast_valid(file, ast)) return "";
     if(ast == NULL) return NULL;
-    if(builder == NULL) return NULL;
+    if(!isValidInfo(info)) return NULL;
     char* temp_name = calloc(10, sizeof(char));
     LLVMValueRef value = NULL;
     switch (ast->tag)
@@ -234,70 +277,70 @@ LLVMValueRef generate_expression(AST *ast, size_t* temp_count, LLVMBuilderRef bu
     case AST_VARIABLE:
         if(ast->data.AST_VARIABLE.is_arg)
         {
-            LLVMModuleRef module = LLVMGetGlobalParent(builder); // TODO: FIX THIS
-            LLVMValueRef func = LLVMGetNamedFunction(module, "main");
-            LLVMValueRef arg = LLVMGetParam(func, ast->data.AST_VARIABLE.arg_index);
+            LLVMValueRef arg = LLVMGetParam(info->function, ast->data.AST_VARIABLE.arg_index);
             return arg;
         }
-        temp_name = realloc(temp_name, 4 + 10);
-        sprintf(temp_name, "temp%ld", *temp_count);
-        (*temp_count)++;
-        temp_name = realloc(temp_name, strlen(temp_name) + 1);
+        sprintf(temp_name, "_t%ld", info->temp_count++);
+        temp_name = realloc(temp_name, sizeof(char) * (strlen(temp_name) + 1));
         value = NULL;
-        for(size_t i = 0; i < sizeof(variables); i++)
+        for(size_t i = 0; i < info->variable_count; i++)
         {
-            if(strcmp(ast->data.AST_VARIABLE.name, LLVMGetValueName(variables[i])) == 0)
+            if(strcmp(ast->data.AST_VARIABLE.name, LLVMGetValueName(info->variables[i])) == 0)
             {
-                value = variables[i];
+                value = info->variables[i];
             }
         }
         if(value == NULL) return NULL;
-        LLVMValueRef ret_value = LLVMBuildLoad(builder, value, temp_name);
+        LLVMValueRef ret_value = LLVMBuildLoad(info->builder, value, temp_name);
         return ret_value;
     case AST_BINARY_OP:
-        return generate_binary_expression(ast, temp_count, builder, variables);
+        return generate_binary_expression(ast, info);
     case AST_CALL:
-        return generate_function_call(ast, temp_count, builder, variables);
+        return generate_function_call(ast, info);
     default:
         printf("Unknown tag: %d\n", ast->tag);
         break;
     }
     return NULL;
 }
-LLVMValueRef generate_binary_expression(AST *ast, size_t* temp_count, LLVMBuilderRef builder, LLVMValueRef* variables)
+LLVMValueRef generate_binary_operation_with_string(char* op, LLVMValueRef left, LLVMValueRef right, char* temp_name, LLVMBuilderRef builder)
 {
-    if(ast == NULL) return NULL;
-    if(builder == NULL) return NULL;
-    if(ast->data.AST_TUPLE.op == NULL){printf("Operator is NULL\n");return NULL;}
-    if(ast->data.AST_TUPLE.left == NULL){printf("Left is NULL\n");return NULL;}
-    if(ast->data.AST_TUPLE.right == NULL){printf("Right is NULL\n");return NULL;}
-    char *op = ast->data.AST_TUPLE.op;
-    LLVMValueRef left = generate_expression(ast->data.AST_TUPLE.left, temp_count, builder, variables);
-    LLVMValueRef right = generate_expression(ast->data.AST_TUPLE.right, temp_count, builder, variables);
-    char* temp_name = calloc(1, sizeof(char*));
-    LLVMValueRef value = NULL;
-    sprintf(temp_name, "t%ld", *temp_count);
-    (*temp_count)++;
-    if(strcmp(op, "+") == 0)
+    if (strcmp(op, "+") == 0)
     {
-        value = LLVMBuildAdd(builder, left, right, temp_name);
+        return LLVMBuildAdd(builder, left, right, temp_name);
     }
-    else if(strcmp(op, "-") == 0)
+    else if (strcmp(op, "-") == 0)
     {
-        value = LLVMBuildSub(builder, left, right, temp_name);
+        return LLVMBuildSub(builder, left, right, temp_name);
     }
-    else if(strcmp(op, "*") == 0)
+    else if (strcmp(op, "*") == 0)
     {
-        value = LLVMBuildMul(builder, left, right, temp_name);
+        return LLVMBuildMul(builder, left, right, temp_name);
     }
-    else if(strcmp(op, "/") == 0)
+    else if (strcmp(op, "/") == 0)
     {
-        value = LLVMBuildSDiv(builder, left, right, temp_name);
+        return LLVMBuildSDiv(builder, left, right, temp_name);
     }
     else
     {
         printf("Unknown operator: %s\n", op);
         return NULL;
     }
+}
+LLVMValueRef generate_binary_expression(AST *ast, InternalInfo *info)
+{
+    if(ast == NULL) return NULL;
+    if(!isValidInfo(info)) return NULL;
+    if(ast->data.AST_TUPLE.op == NULL){printf("Operator is NULL\n");return NULL;}
+    if(ast->data.AST_TUPLE.left == NULL){printf("Left is NULL\n");return NULL;}
+    if(ast->data.AST_TUPLE.right == NULL){printf("Right is NULL\n");return NULL;}
+    char *op = ast->data.AST_TUPLE.op;
+    LLVMValueRef left = generate_expression(ast->data.AST_TUPLE.left, info);
+    LLVMValueRef right = generate_expression(ast->data.AST_TUPLE.right, info);
+    if(left == NULL){printf("Left is NULL\n");return NULL;}
+    if(right == NULL){printf("Right is NULL\n");return NULL;}
+    char* temp_name = calloc(1, sizeof(char*));
+    sprintf(temp_name, "_t%ld", info->temp_count++);
+    LLVMValueRef value = generate_binary_operation_with_string(op, left, right, temp_name, info->builder);
     return value;
 }
